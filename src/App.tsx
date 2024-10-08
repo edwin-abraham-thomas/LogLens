0503111712a5a766a251604e75e02197f800ecfc
Edwin Abraham Thomas <edwinabrahamthomas@outlook.com>
2024-10-08 09:33:48 +0530

    reinitialize project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import { createDockerDesktopClient } from "@docker/extension-api-client";
-import Divider from "@mui/material/Divider";
 import "./styles.css";
-import { Filter } from "./components/Filter";
-import { useEffect, useState } from "react";
-import { FilterCriteria } from "./interfaces/filterCriteria";
-import { LogsContainer } from "./components/LogsContainer";
-import { Box, IconButton, Typography } from "@mui/material";
-import { Constants } from "./constants";
-import RefreshIcon from "@mui/icons-material/Refresh";
+import { Box, Divider, Typography } from "@mui/material";
 
 const client = createDockerDesktopClient();
 function useDockerDesktopClient() {
@@ -15,68 +8,15 @@ function useDockerDesktopClient() {
 }
 
 export function App() {
-  const [filterCriteria, setFilterCriteria] = useState<FilterCriteria>();
-  const [logsRefreshTrigger, setLogsRefreshTrigger] = useState<boolean>(false);
 
-  useEffect(() => {
-    const presetFilterCriteriaString = localStorage.getItem(
-      Constants.FILTER_CRITERIA_LOCAL_STORAGE_KEY
-    );
-    console.log("checking for preset filter");
-    if (
-      presetFilterCriteriaString !== null &&
-      presetFilterCriteriaString !== "" &&
-      presetFilterCriteriaString !== undefined
-    ) {
-      const presetFilter = JSON.parse(presetFilterCriteriaString);
-      console.log("setting preset filter", presetFilter);
-      setFilterCriteria(presetFilter);
-    } else {
-      console.log("setting default filter");
-      setFilterCriteria({
-        selectedContainers: [],
-        stderr: true,
-        stdout: true,
-      });
-    }
-  }, []);
-
-  useEffect(() => {
-    if (filterCriteria === null || filterCriteria === undefined) {
-      return;
-    }
-    console.log("storing into localstorage", filterCriteria);
-    localStorage.setItem(
-      Constants.FILTER_CRITERIA_LOCAL_STORAGE_KEY,
-      JSON.stringify(filterCriteria)
-    );
-  }, [filterCriteria]);
 
   return (
     <Box sx={{ height: "95vh", display: "flex", flexDirection: "column" }}>
       <div className="flex items-center">
         <Typography variant="h2">Log Lens</Typography>
-        <div className="spacer"></div>
-        <IconButton
-          aria-label="filter"
-          onClick={() => setLogsRefreshTrigger(!logsRefreshTrigger)}
-        >
-          <RefreshIcon />
-        </IconButton>
-        {filterCriteria && (
-          <Filter
-            ddClient={useDockerDesktopClient()}
-            setFilterCriteria={setFilterCriteria}
-            filterCriteria={filterCriteria}
-          />
-        )}
       </div>
+      
       <Divider />
-      <LogsContainer
-        ddClient={useDockerDesktopClient()}
-        filterCriteria={filterCriteria}
-        refreshtrigger={logsRefreshTrigger}
-      />
     </Box>
   );
 }
